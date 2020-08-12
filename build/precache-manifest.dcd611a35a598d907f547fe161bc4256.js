7bf9a1a1995b2a9f6eb215a6aca909450c79f701
Renata Cioltea <renatacioltea@gmail.com>
2020-08-12 10:32:56 +0300

    update

---
+++ build/precache-manifest.dcd611a35a598d907f547fe161bc4256.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8981316e2f7b155ffb1b9235ce829a92",
+    "url": "/My-Online-CV/index.html"
+  },
+  {
+    "revision": "d2433e85f7498e2b65ed",
+    "url": "/My-Online-CV/static/css/main.65c87a7a.chunk.css"
+  },
+  {
+    "revision": "bc0b63d95250d7ffa007",
+    "url": "/My-Online-CV/static/js/2.690897ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Online-CV/static/js/2.690897ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2433e85f7498e2b65ed",
+    "url": "/My-Online-CV/static/js/main.b1cea943.chunk.js"
+  },
+  {
+    "revision": "9b71128714a48a385df1",
+    "url": "/My-Online-CV/static/js/runtime-main.6902a77b.js"
+  },
+  {
+    "revision": "074d829b2f957b84f743b026e22b31a0",
+    "url": "/My-Online-CV/static/media/about-me.074d829b.png"
+  },
+  {
+    "revision": "c40bb38b3b187d883341a512e6692104",
+    "url": "/My-Online-CV/static/media/drum-kit.c40bb38b.JPG"
+  },
+  {
+    "revision": "f06c6d4d6d6efcd3b8a5a74caa225f13",
+    "url": "/My-Online-CV/static/media/my-profile-picture.f06c6d4d.png"
+  },
+  {
+    "revision": "b4e6c925c06bcc47d1f96d685dedf990",
+    "url": "/My-Online-CV/static/media/projects.b4e6c925.png"
+  },
+  {
+    "revision": "2fb7e76b8b37ac22d33d0d0ce9dee84f",
+    "url": "/My-Online-CV/static/media/tic-tac-toe.2fb7e76b.png"
+  },
+  {
+    "revision": "482776bb35ed3d9271fed9e0f7abdca8",
+    "url": "/My-Online-CV/static/media/weather-app_s.482776bb.png"
+  }
+]);
